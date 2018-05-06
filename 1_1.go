1d4960d052c6b42707506163b88c1b4bae634b04
rerost <gmailii@yahoo.co.jp>
2018-05-06 19:55:23 +0900

    Add init 1_1

---
+++ 1_1.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+  "log"
+  "net/http"
+)
+
+func main() {
+  http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+    w.Write([]byte(`
+      <html>
+        <head>
+          <title>チャット</title>
+        </head>
+        <body>
+          チャットをしましょう
+        </body>
+      </html`))
+    })
+    if err := http.ListenAndServe(":8080", nil); err != nil {
+      log.Fatal("ListenAndService:", err)
+    }
+}
